d751227086d63410e737b3dcc02628badf5839c7
Smart <smartmunachi79@gmail.com>
2025-12-03 13:36:08 +0100

    bew

--- src/theme/About.jsx
+++ src/theme/About.jsx
@@ -6,7 +6,7 @@ function About() {
   let {theme} = useContext(Emmy)
   return (
     <div>
-     <div className={`h-dvh w-full duration-200 ${theme ? "bg-amber-300 text-black" : "bg-amber-800 text-white"}`}>
+     <div className={`h-screen w-full duration-200 ${theme ? "bg-amber-300 text-black" : "bg-amber-800 text-white"}`}>
        <h1 className='text-2xl'>About us</h1>
      </div>
     </div>
